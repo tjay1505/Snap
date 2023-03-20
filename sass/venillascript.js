4dc5af46144861c64a5964e1edb37a5cfdb10715
tjay1505 <119523945+tjay1505@users.noreply.github.com>
2023-03-20 13:16:33 +0530

    mon 20/3 1:00 pm

---
+++ sass/venillascript.js
@@ -0,0 +1,16 @@
+function openOne() {
+    var x = document.getElementById("oone");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+  function opentwo() {
+    var x = document.getElementById("ttwo");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
